1dec7d4526acf5ab161e7bbac478713656766459
Jordan Forde <S00181590@mail.itsligo.ie>
2019-02-27 15:03:01 +0000

    Add project files.

---
+++ Exercise5/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string StudentNumber{get;set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public Student(String name, string number, string mail, string phone)
+        {
+            Name = name;
+            StudentNumber = number;
+            Email = mail;
+            Phone = phone;
+        }
+    }
+}
